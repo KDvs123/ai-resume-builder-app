42989d01c50689acc595189860a2dc28919f39a4
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-07-03 10:01:40 +0530

    added the clerk for the authentication

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import SigninPage from './auth/sign-in/index.jsx'
 import Dashboard from './dashboard/index.jsx'
 import Home from './home/index.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 const router=createBrowserRouter([
   {
     
@@ -31,8 +33,10 @@ const router=createBrowserRouter([
   }
 ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </React.StrictMode>
+);
